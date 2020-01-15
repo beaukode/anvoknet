3f49c1fb9458d243ac8cfdab6225c6f89eaf9702
beaukode <5773637+beaukode@users.noreply.github.com>
2020-01-15 09:55:22 +0100

    Utilisation de gtag pour GA

--- src/StatsTracking.tsx
+++ src/StatsTracking.tsx
@@ -4,11 +4,12 @@ import { useLocation } from "react-router-dom";
 const StatsTracking: React.FC = () => {
   const location = useLocation();
   React.useEffect(() => {
-    if (window && (window as any).ga) {
-      (window as any).ga("set", "page", location.pathname);
-      (window as any).ga("send", "pageview");
+    if (typeof (window as any).gtag == "function") {
+      (window as any).gtag("config", "UA-132856623-2", {
+        page_path: location.pathname,
+      });
     }
-  }, [location]);
+  }, [location.pathname]);
   return null;
 };
 
